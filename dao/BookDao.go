85b60006482d617d7b7c489e4840e65842af9690
huangsn <huangsen@outlook.com>
2022-02-15 17:48:23 +0800

    add struct

---
+++ dao/BookDao.go
@@ -0,0 +1,41 @@
+package dao
+
+import "time"
+
+type BookDao struct {
+	// 主键ID
+	Id string
+
+	// 书名
+	Book string
+
+	// 作者
+	Author string
+
+	// 出版社
+	Publisher string
+
+	// ISBN
+	Isbn string
+
+	// 图书分类
+	Classification string
+
+	// 所属楼层
+	Floor int8
+
+	// 书架号码
+	Bookshelf int8
+
+	// 购买日期
+	DateOfPurchase time.Time
+
+	// 备注
+	Remarks string
+
+	// 创建时间
+	CreateTime time.Time
+
+	// 更新时间
+	UpdateTime time.Time
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module LibraryManageSys
+
+go 1.17
